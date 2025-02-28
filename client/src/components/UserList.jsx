82fd1d7760cfe80198faea8d0b94250d92238f80
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-02-28 20:48:54 +0200

    Save new user

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -24,13 +24,36 @@ export default function UserList() {
 
     const closeCreateUserClickHandler = () => {
         setShowCreate(false);
+    };
+
+    const saveCreateUserClickHandler = async (e) => {
+        // Stop default refresh behaviour
+        e.preventDefault();
+
+        // Get form date
+        const formData = new FormData(e.target);
+        const userData = Object.fromEntries(formData);
+
+        // create new user on server
+        const newUser = await userService.create(userData);
+        
+        // update local state
+        setUsers(state => [...state, newUser])
+
+        // close modal 
+        setShowCreate(false);
     }
 
     return (
         <section className="card users-container">
             <Search />
 
-            {showCreate && <UserCreate onClose={closeCreateUserClickHandler} />}
+            {showCreate && (
+                <UserCreate
+                    onClose={closeCreateUserClickHandler}
+                    onSave={saveCreateUserClickHandler}
+                />)
+            }
 
             <div className="table-wrapper">
                 <div className="overlays">
